e43ae275000a3657b413b0f48e51a323a49a098e
Felipe Flores <felipeflores@pop-os.localdomain>
2020-09-16 17:31:59 -0600

    agregada funcionalidad de la bala aunque con un bug

--- Scripts_pygame/pygame_script.py
+++ Scripts_pygame/pygame_script.py
@@ -9,9 +9,11 @@ CONFIGURACIÓN GENERAL
 pygame.init()
 
 # Se define el modo del display/ventana
-ancho = 800
+ancho = 600
 altura = 600
 screen = pygame.display.set_mode((ancho, altura))
+background = pygame.image.load("graficos/fondo.jpg")
+background = pygame.transform.scale(background, (ancho, altura)) # Reescalamiento
 
 # Título e ícono
 pygame.display.set_caption("Mi primer juego")
@@ -19,7 +21,10 @@ icon = pygame.image.load("graficos/spaceship.png")
 pygame.display.set_icon(icon)  # No funcionará dependiendo del tema que usen en su SO
 
 # Parámetros iniciales de objetos del juego
-# Jugador
+# Configuración
+game_speed = 2
+
+# Jugadores
 player_img = pygame.image.load("graficos/spaceship_player.png")
 size = player_img.get_size()
 playerX = ancho/2 - size[0]/2  # La posición debe tomar en cuenta el tamaño de 
@@ -32,6 +37,9 @@ alien_img = pygame.image.load("graficos/alien.png")
 alienX = random.randint(0, ancho - size[0])
 franja_enemigos = random.randint(round(size[1]/2), 0.2*altura)
 
+# Proyectil
+bullet_img = pygame.image.load("graficos/bullet.png")
+
 # Controlador del while para apertura y cerradura de la ventana
 running = True
 
@@ -42,12 +50,12 @@ ELEMENTOS Y SISTEMAS DEL JUEGO
 # Clases 
 class Flotador:
 
-    def __init__(self, x, y, img, mov_rate = 0.3, speedX = 0):
+    def __init__(self, x, y, img, mov_rate = 1.5, speedX = 0):
         self.img = img
         self.size = self.img.get_size()
         self.x, self.y = x, y
         self.speedX = speedX
-        self.mov_rate = mov_rate
+        self.mov_rate = mov_rate*game_speed
         self.actualizar_posicion()
 
     def actualizar_posicion(self):
@@ -57,31 +65,60 @@ class Flotador:
     def check_boundary(self):
         borde_izquierdo = 0
         borde_derecho = ancho - self.size[0]
-        xPosition = self.x
-        if xPosition <= borde_izquierdo:
+        if self.x <= borde_izquierdo:
             self.x = borde_izquierdo
-        elif xPosition >= borde_derecho:
+        elif self.x >= borde_derecho:
             self.x = borde_derecho
 
 class Enemigo(Flotador):
 
-    def __init__(self, x, y, img, mov_rate = 0.3, speedX = 0.5):
-        super().__init__(x, y, img, mov_rate, speedX = speedX)
+    def __init__(self, x, y, img, speedX = None, approaching_speed = 0.05):
+        if speedX is not None:
+            self.speedX = random.choice([-speedX, speedX])
+        else:
+            self.speedX = random.choice([-game_speed, game_speed])
+        super().__init__(x, y, img, speedX = self.speedX)
+        self.approaching_speed = approaching_speed 
 
     def check_boundary(self):
         borde_izquierdo = 0
         borde_derecho = ancho - self.size[0]
         xPosition = self.x
         if xPosition <= borde_izquierdo:
-            self.speedX = self.mov_rate
+            self.speedX = - self.speedX
+            self.y += self.approaching_speed*altura 
         elif xPosition >= borde_derecho:
-            self.speedX = -self.mov_rate
+            self.speedX = - self.speedX
+            self.y += self.approaching_speed*altura 
 
+class Bala(Flotador):
+
+    def __init__(self, x, y, img, emisor, speedX = 0):
+        self.disparado = False
+        self.emisor = emisor
+        super().__init__(x, y, img, speedX = speedX)
+
+    def fire(self):
+        self.disparado = True
+        x = self.x + self.emisor.size[0]/2 - self.size[0]/2 # ajuste a los tamaños del
+                                                            # emisor de la bala
+        screen.blit(self.img, (x, self.y))
+
+    def actualizar_posicion(self):
+        if self.disparado:
+            self.y -= self.mov_rate
+            self.fire()
+        # Una vez que la bala llega al final de la pantalla
+        # regresamos su estado a la posicion del cohete y disparado = False
+        if self.y <= 0:  
+            self.disparado = False
+            self.x, self.y = self.emisor.x, self.emisor.y
 
 
 # Inicialización de datos y objetos:
 cohete = Flotador(playerX, playerY, player_img)
 alien = Enemigo(alienX, 0.2*altura, alien_img)
+bala_normal = Bala(playerX, playerY, bullet_img, cohete)
 
 # Ciclo de repetición del juego
 while running:
@@ -104,6 +141,8 @@ while running:
                 cohete.speedX = -cohete.mov_rate
             if event.key == pygame.K_RIGHT:
                 cohete.speedX = +cohete.mov_rate
+            if event.key == pygame.K_SPACE:
+                bala_normal.fire()
 
         # KEYUP: Reacción a teclas levantadas
         key = pygame.key.get_pressed()
@@ -122,11 +161,13 @@ while running:
     - Posición del jugador
     """
     # Fondo: Red, Green, Blue
-    screen.fill((0, 140, 205))
+    #screen.fill((0, 140, 205))
+    screen.blit(background, (0,0))
 
     # Cambio de estado de los objetos
     cohete.actualizar_posicion()
     alien.actualizar_posicion()
+    bala_normal.actualizar_posicion()
 
     # Colisiones con el borde de la pantalla
     cohete.check_boundary()
