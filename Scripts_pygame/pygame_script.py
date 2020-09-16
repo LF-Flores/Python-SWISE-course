db77c706af4a4fc66695fd16c542c3cf0bd40d58
Felipe Flores <felipeflores@pop-os.localdomain>
2020-09-16 16:40:06 -0600

    Agregado cohete

--- Scripts_pygame/pygame_script.py
+++ Scripts_pygame/pygame_script.py
@@ -5,15 +5,24 @@ import pygame
 pygame.init()
 
 # Se define el modo del display/ventana
-screen = pygame.display.set_mode((600, 600))
+screen = pygame.display.set_mode((800, 600))
 
 # Título e ícono
 pygame.display.set_caption("Mi primer juego")
 icon = pygame.image.load("graficos/spaceship.png") 
 pygame.display.set_icon(icon)  # No funcionará dependiendo del tema que usen en su SO
 
+# Posición inicial del jugador
+player_img = pygame.image.load("graficos/spaceship_player.png")
+playerX = 400 - 64/2  # La posición debe tomar en cuenta el tamaño de la imagen en sí
+playerY = 480
+
 # Controlador del while para apertura y cerradura de la ventana
 running = True
+
+def player(x,y):
+    screen.blit(player_img, (x,y))
+
 
 # Ciclo de repetición del juego
 while running:
@@ -24,8 +33,13 @@ while running:
         if event.type == pygame.QUIT: 
             running = False
 
+    ## Actualización de la pantalla
     # Red, Green, Blue
-    screen.fill((160, 0, 0))
-    pygame.display.update()    
+    screen.fill((0, 140, 205))
+
+    # acción del jugador
+    player(playerX, playerY)
+    pygame.display.update() 
+
     
             
